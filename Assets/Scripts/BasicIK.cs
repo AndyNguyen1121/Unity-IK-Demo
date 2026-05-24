7e4a4f7a2159a1cd13b8fab5091e05eae91d3176
AndyNguyen1121 <andyhoangnguyen2005@gmail.com>
2026-05-23 22:30:49 -0700

    added more scenes

---
+++ Assets/Scripts/BasicIK.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BasicIK : MonoBehaviour
+    {
+        [Range(0, 1)]
+        public float weight;
+
+        [SerializeField] private GameObject handle;
+        [SerializeField] private Animator animator;
+        private void OnAnimatorIK(int layerIndex)
+        {
+            // set rotation and position
+            animator.SetIKPosition(AvatarIKGoal.RightHand, handle.transform.position);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, handle.transform.rotation);
+            
+            // Set weight
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, weight);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, weight);
+        }
+    }
+}
--- Assets/Scripts/IKDemo.cs
+++ Assets/Scripts/IKDemo.cs
@@ -6,66 +6,92 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
 public class IKDemo : MonoBehaviour
 {
-    [SerializeField] private Transform rightHandTarget;
+    [Header("Assign References")]
+    [SerializeField] private Transform target;
     [SerializeField] private Animator animator;
-    [FormerlySerializedAs("grabOffset")] [SerializeField] private Vector3 rightGrabOffset;
+    
+    [Header("Right Hand")]
+    [SerializeField] private Vector3 rightGrabOffset;
+    [SerializeField] private Vector3 rightHandRotationOffset;
+    
+    [Header("Left Hand")]
     [SerializeField] private Vector3 leftGrabOffset;
-    [FormerlySerializedAs("handRotationOffset")] [SerializeField] private Vector3 rightHandRotationOffset;
-    [FormerlySerializedAs("leftGrabRotationOffset")] [SerializeField] private Vector3 leftHandRotationOffset;
+    [SerializeField] private Vector3 leftHandRotationOffset;
+    
+    // When the object snaps to the hand, the animation also snaps back into its original position
+    // this is used to smoothly transition the animation with the IK
+    private Vector3 placeholder;
+    private Quaternion placeholderRotation;
 
     [SerializeField] private Vector3 grabbedObjectOffset;
+
+    private void Start()
+    {
+        placeholder = target.position;
+        placeholderRotation = target.rotation;
+    }
+
     private void OnAnimatorIK(int layerIndex)
     {
-        Vector3 finalGrabOffset = rightHandTarget.forward * rightGrabOffset.z;
-        finalGrabOffset += rightHandTarget.right * rightGrabOffset.x;
-        finalGrabOffset += rightHandTarget.up * rightGrabOffset.y;
+        Vector3 placeholderForward = placeholderRotation * Vector3.forward;
+        Vector3 placeholderRight = placeholderRotation * Vector3.right;
+        Vector3 placeholderUp = placeholderRotation * Vector3.up;
 
-        Quaternion rightHandFinalRotOffset = Quaternion.Euler(rightHandRotationOffset);
-        
-        animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandTarget.position + finalGrabOffset);
+        Vector3 finalGrabOffset = placeholderForward * rightGrabOffset.z + placeholderRight   * rightGrabOffset.x + placeholderUp * rightGrabOffset.y;
+
+        // Set position and rotation based on the weight defined in animation curves
+        animator.SetIKPosition(AvatarIKGoal.RightHand, placeholder + finalGrabOffset);
         animator.SetIKPositionWeight(AvatarIKGoal.RightHand, animator.GetFloat("IK_RightHand_Position_Weight"));
-        animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandTarget.rotation * rightHandFinalRotOffset);
+        animator.SetIKRotation(AvatarIKGoal.RightHand, placeholderRotation * Quaternion.Euler(rightHandRotationOffset));
         animator.SetIKRotationWeight(AvatarIKGoal.RightHand, animator.GetFloat("IK_RightHand_Rotation_Weight"));
-        
-        Vector3 finalLeftGrabOffset = rightHandTarget.forward * leftGrabOffset.z;
-        finalLeftGrabOffset += rightHandTarget.right * leftGrabOffset.x;
-        finalLeftGrabOffset += rightHandTarget.up * leftGrabOffset.y;
-        
-        Quaternion leftHandFinalRotOffset = Quaternion.Euler(leftHandRotationOffset);
-        
-        animator.SetIKPosition(AvatarIKGoal.LeftHand, rightHandTarget.position + finalLeftGrabOffset);
+
+        Vector3 finalLeftGrabOffset = placeholderForward * leftGrabOffset.z + placeholderRight * leftGrabOffset.x  + placeholderUp * leftGrabOffset.y;
+
+        animator.SetIKPosition(AvatarIKGoal.LeftHand, placeholder + finalLeftGrabOffset);
         animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, animator.GetFloat("IK_LeftHand_Position_Weight"));
-        animator.SetIKRotation(AvatarIKGoal.LeftHand, rightHandTarget.rotation * leftHandFinalRotOffset);
+        animator.SetIKRotation(AvatarIKGoal.LeftHand, placeholderRotation * Quaternion.Euler(leftHandRotationOffset));
         animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, animator.GetFloat("IK_LeftHand_Rotation_Weight"));
 
     }
 
-    public void PickUpItem()
+    // called in animation event
+    public void PickUpItem(int handIndex)
     {
-        StartCoroutine(SmoothAttach(rightHandTarget, animator.GetBoneTransform(HumanBodyBones.LeftHand)));
+        if (handIndex == 0)
+        {
+            StartCoroutine(SmoothAttach(target, animator.GetBoneTransform(HumanBodyBones.LeftHand)));
+        }
+        else
+        {
+            StartCoroutine(SmoothAttach(target, animator.GetBoneTransform(HumanBodyBones.RightHand)));
+        }
     }
     
+    // gravitates item towards hand
     public IEnumerator SmoothAttach(Transform item, Transform hand)
     {
-        
         Vector3 startPos = item.position;
         float t = 0f;
         float duration = 0.25f;
+        
 
         while (t < 1f)
         {
-            t += Time.deltaTime / duration;
+            t = Mathf.Clamp01(t + Time.deltaTime / duration);
+            float smooth = Mathf.SmoothStep(0f, 1f, t);
 
-            item.position = Vector3.Lerp(startPos, hand.position, t);
-            item.up = hand.forward;
+            Vector3 worldOffset = hand.TransformDirection(grabbedObjectOffset); 
+            item.position = Vector3.Lerp(startPos, hand.position + worldOffset, smooth);
+
             yield return null;
         }
-        item.SetParent(hand, false);
-        item.up = hand.forward;
+        item.SetParent(hand,false);
+        item.rotation = Quaternion.LookRotation(-hand.right, -hand.forward);
         item.localPosition = grabbedObjectOffset;
     }
 }
